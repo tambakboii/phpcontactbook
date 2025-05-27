a7a76af297a1f206e7ae6a813bd59e454092d6c7
Yoppy Yunhasnawa <yunhasnawa@ymail.com>
2025-05-27 10:11:40 +0700

    Ganti warna background menjadi biru

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ $result = $conn->query($sql);
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
-            background-color: deeppink;
+            background-color: aqua;
         }
         .container {
             max-width: 80%;
